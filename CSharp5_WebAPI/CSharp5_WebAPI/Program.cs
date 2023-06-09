3bee593910eed425dd5ef32497cab33b7686a4f9
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2023-06-09 17:20:48 +0700

    addTransient lan cuoi

--- CSharp5_WebAPI/CSharp5_WebAPI/Program.cs
+++ CSharp5_WebAPI/CSharp5_WebAPI/Program.cs
@@ -14,6 +14,19 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<INationalServices, NationalServices>();
 builder.Services.AddTransient<IVoucherServies, VoucherServies>();
 builder.Services.AddTransient<IRoleServices, RoleServices>();
+builder.Services.AddTransient<IPostServices, PostServices>();
+builder.Services.AddTransient<IProductServices, ProductServices>();
+builder.Services.AddTransient<IProducerServices,ProducerServices>();
+builder.Services.AddTransient<IBillServices, BillServices>();
+builder.Services.AddTransient<IBillDetailServices, BillDetailServices>();
+builder.Services.AddTransient<IChefServices, ChefServices>();
+builder.Services.AddTransient<ICategorieSerivces, CategorieServices>();
+builder.Services.AddTransient<IUserServices, UserServices>();
+builder.Services.AddTransient<ICartServices, CartServices>();
+builder.Services.AddTransient<ICartDetailServices, CartDetailServices>();
+builder.Services.AddTransient<IComboServices, ComboServices>();
+builder.Services.AddTransient<IComboItemServices, ComboItemServices>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
