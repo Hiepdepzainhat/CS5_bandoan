23976e19f900a6845848f8a35575d9464b5730cb
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2023-06-13 03:33:42 +0700

    doi cho

--- CSharp5_WebAPI/CSharp5_Web_Client/Program.cs
+++ CSharp5_WebAPI/CSharp5_Web_Client/Program.cs
@@ -38,7 +38,7 @@ app.UseEndpoints(endpoints =>
     endpoints.MapAreaControllerRoute(
     name: "AdminHome",
     areaName: "Admin",
-    pattern: "Admin/{controller=AdminHome}/{action=Index}/{id?}"
+    pattern: "Admin/{controller=ProducerAdmin}/{action=GetProducer}/{id?}"
     );
     endpoints.MapAreaControllerRoute(
     name: "CustomerHome",
