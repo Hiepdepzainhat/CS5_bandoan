e066ec00a2036493a89dd063b33e0a6a2e9b8330
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2023-06-12 21:45:31 +0700

    them areas

---
+++ CSharp5_WebAPI/CSharp5_WebAPI/Viewmodels/ProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CSharp5_Share.Models;
+
+namespace CSharp5_WebAPI.Viewmodels
+{
+    public class ProductViewModel
+    {
+        public Guid ProductID { get; set; }
+        public Guid? CategoryID { get; set; }
+        public Guid? ProducerID { get; set; }
+        public Guid? ChefID { get; set; }
+        public Guid? IdVoucher { get; set; }
+        public int PercentageDiscount { get; set; }
+        public string? ProductName { get; set; }
+        public int? Quantity { get; set; }
+        public int? EntryPrice { get; set; }
+        public DateTime? DateOfManufacture { get; set; }
+        public DateTime? Expired { get; set; }
+        public DateTime? ImPortDate { get; set; }
+        public int? Price { get; set; }
+        public int? Status { get; set; }
+        public string? Desciption { get; set; }
+        public int? LastPrice => (int)(Price * PercentageDiscount)/100;
+    }
+}
